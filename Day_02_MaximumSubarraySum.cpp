8a111279d69613afb0d79dbf46e085e23041fdd9
Rajveer Pandey <66234228+rajveer97@users.noreply.github.com>
2022-06-06 20:04:33 +0530

    Day 02 Solution

---
+++ Day_02_MaximumSubarraySum.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    long long sum = 0;
+    if(n==0) return 0;
+    if(n==1) return arr[0];
+    long long maxSum = 0;
+    
+    for(int i = 0; i<n; i++) {
+        sum += arr[i];
+        maxSum = max(sum, maxSum);
+        
+        if(sum < 0) {
+            sum = 0;
+        }
+    }
+    return maxSum; 
+}
