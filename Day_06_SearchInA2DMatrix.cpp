d06a14ca071b8cf8e3f081f05650de9e9516e075
Rajveer Pandey <kram35477@gmail.com>
2022-06-10 23:48:27 +0530

    Day 06 Optimized solution added

--- Day_06_SearchInA2DMatrix.cpp
+++ Day_06_SearchInA2DMatrix.cpp
@@ -1,8 +1,9 @@
+// Brut-force Approch
 #include <bits/stdc++.h> 
 #include <vector>
 
 bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
-    // brut-force approch
+    
     for(int i=0; i<m; i++) {
         for(int j=0; j<n; j++) {
             if(mat[i][j] == target)
@@ -11,3 +12,24 @@ bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target
     }
     return false;
 }
+
+// Optimized Approch
+#include <bits/stdc++.h> 
+#include <vector>
+
+bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
+    // Optimized approch
+    int i = 0, j = n-1;
+    
+    while(i < m && j >= 0) {
+        if(mat[i][j] == target) return true;
+        else if(mat[i][j] > target) {
+            j--;
+        }
+        else {
+            i++;
+        }
+    }
+    return false;
+}
+
