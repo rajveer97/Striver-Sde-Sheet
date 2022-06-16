0f4095ce6d36085ba1a16b8cc3f7ae1ae0367693
Rajveer Pandey <kram35477@gmail.com>
2022-06-16 23:01:15 +0530

    Day 12 Solution

---
+++ Day_12_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+int uniqueSubstrings(string input)
+{
+    vector<int> freqChar(26, 0); // for marking visited
+    int maxLength = 1;
+    int i=0, j=0, n = input.size();
+    
+    while(j<n)
+    {
+        if(freqChar[input[j] - 'a'] == 0) {
+            freqChar[input[j] - 'a'] = 1;
+        }
+        else {
+            maxLength = max(maxLength, j-i);
+            while(i <= j && freqChar[input[j] - 'a'] > 0) {
+                freqChar[input[i] - 'a'] = 0;
+                i++;
+            }
+            freqChar[input[j] - 'a'] = 1;
+        }
+        j++;
+    }
+    
+    maxLength = max(maxLength, j-i);
+    return maxLength;
+}
