a2b82b072eb67b0361444af2a9188d2a7d2814e0
Rajveer Pandey <66234228+rajveer97@users.noreply.github.com>
2022-08-08 14:06:51 +0530

    Update Day_53_5_LongestIncreasingSubsequence.cpp

--- Day_53_5_LongestIncreasingSubsequence.cpp
+++ Day_53_5_LongestIncreasingSubsequence.cpp
@@ -1,4 +1,4 @@
-// Method 1: DP Tabulation (LeetCode)
+// Method 0: Brute-force (LeetCode)
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
@@ -15,6 +15,94 @@ public:
         return *max_element(lis, lis + nums.size());
     }
 };
+
+//-----------------------------------------------------------
+
+// Method 1: Recursion
+class Solution {
+public:
+    int solve(int index, int prev_index, vector<int> &nums)
+    {
+        if(index == nums.size()) return 0;
+        
+        // Not Take
+        int len = 0 + solve(index+1, prev_index, nums);
+            
+        // Take    
+        if(prev_index == -1 || nums[index] > nums[prev_index]) 
+            len = max(len, 1+solve(index+1, index, nums));
+            
+        
+        return len;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        return solve(0, -1, nums);
+    }
+};
+
+//------------------------------------------------------------------------------
+
+// Method 2: Memoization
+class Solution {
+public:
+    int solve(int index, int prev_index, vector<int> &nums,vector<vector<int>>& dp)
+    {
+        if(index == nums.size()) return 0;
+        
+        if(dp[index][prev_index+1] != -1)
+            return dp[index][prev_index+1];
+        
+        // Not Take
+        int len = 0 + solve(index+1, prev_index, nums, dp);
+            
+        // Take    
+        if(prev_index == -1 || nums[index] > nums[prev_index]) 
+            len = max(len, 1+solve(index+1, index, nums, dp));
+            
+        
+        return dp[index][prev_index+1] = len;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<vector<int>> dp(n, vector<int> (n+1, -1)); //n+1,bcz we are shifting 1 prev_index 
+        
+        return solve(0, -1, nums, dp);
+    }
+};
+
+//------------------------------------------------------------------------------
+
+// Method 3: Tabulation DP
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int> (n+1, 0));
+        
+        for(int index = n-1; index >= 0; index--)
+        {
+            for(int prev_index = index-1; prev_index >= -1; prev_index--)
+            {
+                // Not Take
+                int len = 0 + dp[index + 1][prev_index + 1];
+
+                // Take    
+                if(prev_index == -1 || nums[index] > nums[prev_index]) 
+                    len = max(len, 1 + dp[index + 1][index + 1]);
+
+
+                dp[index][prev_index+1] = len;             
+            }
+        }
+        
+        
+        return dp[0][-1+1];
+    }
+};
+
 
 //-----------------------------------------------------------
 
