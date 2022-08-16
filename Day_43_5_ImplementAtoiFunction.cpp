e1f352d88adc6866e4134433ed96f140bc0e5647
Rajveer Pandey <66234228+rajveer97@users.noreply.github.com>
2022-08-16 11:56:43 +0530

    Update code

--- Day_43_5_ImplementAtoiFunction.cpp
+++ Day_43_5_ImplementAtoiFunction.cpp
@@ -1,4 +1,35 @@
 // atoi() -> string to int
+
+// Leetcode
+class Solution {
+public:
+    int myAtoi(string str) 
+    {
+        if (str.empty()) return 0;
+        int i = 0, sign = 1;
+
+        while (i + 1 < str.size() && isspace(str[i])) 
+            ++i;            // ignore leading white space
+
+        long res = 0;
+        if (str[i] == '-' || str[i] == '+')    // check if number +ve or -ve
+            sign = (str[i++]=='-') ? -1 : 1;
+
+        
+        while (i < str.size()) 
+        {
+            if (isdigit(str[i])) res = 10 * res + str[i++] - '0';
+            else return res * sign;
+
+            if (res > INT_MAX) return sign == -1 ? INT_MIN : INT_MAX;
+        }
+        return res * sign;
+    }
+};
+
+
+//--------------------------------------------------------------------------------------
+
 int atoi(string str) {
     // Write your code here.
     int result = 0;
